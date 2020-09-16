6bc66ec14712a60dbf2d5810a5ed9206b3460131
maciej-123 <68496798+maciej-123@users.noreply.github.com>
2020-09-16 20:51:03 +0100

    Update Altcode_Unicode_Example.ino

--- examples/Altcode_Unicode_Example/Altcode_Unicode_Example.ino
+++ examples/Altcode_Unicode_Example/Altcode_Unicode_Example.ino
@@ -1,6 +1,6 @@
 //alt code_unicode tester - Maciej Zajaczkowski, copyright 2020 ©
 
-//WARNING: please ensure that unicodeinput.exe is open and that numlock is set to allow numbers to be inputted.
+//WARNING: please ensure that unicodeinput.exe is open. Functions automatically set numlock position.
 //otherwise may behave unpredictably. Also lease note that UNICODE Alt+X only takes up to 4 digit hex codes and
 //only works in some applications such as Microsoft Word and not google chrome.
 
@@ -35,33 +35,33 @@ void loop() {
     {
       //insert alt code
       //put -1 for omission of number
-      alt_code(-1,2,1,9);
+      //alt_code(-1,2,1,9);
       //Unicode_Alt_X("2ef3");
-      //Unicode_Alt_Plus("2ef3"); //chinese
+      Unicode_Alt_Plus("2ef3"); //chinese
     }
     if(digitalRead(button2) == HIGH)
     {
       //insert alt code
       //put -1 for omission of number      
-      alt_code(-1,1,7,8); 
+      //alt_code(-1,1,7,8); 
       //Unicode_Alt_X("2230");
-      //Unicode_Alt_Plus("2230"); //mathematics
+      Unicode_Alt_Plus("2230"); //mathematics
     }
     if(digitalRead(button3) == HIGH)
     {
       //insert alt code
       //put -1 for omission of number
-      alt_code(-1,1,7,7);
+      //alt_code(-1,1,7,7);
       //Unicode_Alt_X("26a1");
-      //Unicode_Alt_Plus("26d0");  //emoji
+      Unicode_Alt_Plus("26d0");  //emoji
     }
     if(digitalRead(button4) == HIGH)
     {
       //put -1 for omission of number
       //Unicode_Alt_X("26a1"); 
-      alt_code(-1,1,7,6);  
+      //alt_code(-1,1,7,6);  
       //only Unicode Alt + can take 5 digit codes
-      //Unicode_Alt_Plus("1203B"); //cuneiform
+      Unicode_Alt_Plus("1203B"); //cuneiform
       //this may not display on some applications but it should in chrome     
       
     }
