fd2d97149391ef675b5583cb7ad696c0cbe72174
maciej-123 <68496798+maciej-123@users.noreply.github.com>
2020-09-16 20:47:51 +0100

    Update Altcode_Unicode_lib.h

--- Altcode_Unicode_lib.h
+++ Altcode_Unicode_lib.h
@@ -2,10 +2,11 @@
 #define ALTCODE_UNICODE_LIB_H
 
 #include <Arduino.h>
-#include <Keyboard.h>
+#include <HID-Project.h>
 
 void alt_code(int num1, int num2, int num3, int num4);
 void Unicode_Alt_Plus(String code);
 void Unicode_Alt_X(String code);
+void Keypad_press(int num);
 
 #endif 
